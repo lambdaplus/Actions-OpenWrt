c04d90b00a3c4f42fae0c1736d579cada4aa1fb8
lambdaplus <bond010418@gmail.com>
2022-09-08 07:31:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,6 +26,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-cat >> feeds.conf.default <<EOF
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-EOF
+#cat >> feeds.conf.default <<EOF
+#src-git kenzo https://github.com/kenzok8/openwrt-packages
+#EOF
